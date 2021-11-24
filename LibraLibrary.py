309c0cb728345ac9cda0d43d3c80b46af096bd47
Shkolenko Denys <69078357+Shkolenko-Denys@users.noreply.github.com>
2021-11-24 21:02:56 +0200

    LibraLibrary updated

--- LibraLibrary.py
+++ LibraLibrary.py
@@ -1,16 +1,20 @@
 import mysql.connector
 from sqlalchemy import create_engine
+from config import *
 
 
 class LibraLibrary:
     """LibraLibrary."""
 
     def __init__(self):
-        self.library_db = mysql.connector.connect(host="localhost",
-                                                  user="root",
-                                                  password="SoWhat_4")
+        self.library_db = mysql.connector.connect(
+            host=library_db_host,
+            user=library_db_user,
+            password=library_db_password
+        )
         self.engine = create_engine(
-            "mysql+pymysql://root:SoWhat_4@localhost/library")
+            f"{library_db_dialect}+{library_db_driver}://{library_db_user}:"
+            f"{library_db_password}@{library_db_host}/{library_db_name}")
         self.connection = self.engine.connect()
 
     def login(self):
